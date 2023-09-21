5a11de5eefd84660f059ff76826580cf55ee4ac8
murulii <muruli800@gmail.com>
2023-09-22 00:47:44 +0530

    Create install.sh

---
+++ kind install on vm/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+hostnamectl set-hostname Kind
+
+     # Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+ # Add the repository to Apt sources:
+echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker --version
+
+sudo usermod -aG docker $USER
+
+ # For AMD64 / x86_64
+ [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+snap install kubectl --classic
+  
+kubectl version
+kind create cluster
+
+kind get clusters
+kubectl get no
+
+
+nano kind-config
+#######
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+###########
+
+kind create cluster --name=kubernetes --config=kind-config
+kubectl get nodes
